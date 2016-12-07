c65bd86fa32fbc8bf8c4b48812df295a7fdefc76
GnarGnar <kburke@csumb.edu>
2016-12-07 10:46:54 -0800

    user key encryption

--- encryption.py
+++ encryption.py
@@ -1,27 +1,45 @@
 from Crypto.Cipher import AES
 import os, binascii
 
-rand_string = binascii.b2a_hex(os.urandom(16))
-rand_string2 = os.urandom(16)
 
-message =input('Please enter your message: ')
-length = 16 - (len(message)%16)
-x = len(message)
-for x in range (length):
-	message += " "
+
+def encryption():
+	
+	message =input('Please enter your message: ')
+	length = 16 - (len(message)%16)
+	x = len(message)
+	for x in range (length):
+		message += " "
+	
+	key = input('Please enter your key: ')
+	keyLength = 16 - (len(key)%16)
+	x = len(key)
+	for x in range(keyLength):
+		key += " "
+	print(key)
+	
+	obj = AES.new(key,AES.MODE_CBC,'This is an IV456')
+	ciphertext = obj.encrypt(message)
+	print("This is the encrypted message: ", ciphertext)
 	
 	
-obj = AES.new(rand_string,AES.MODE_CBC,rand_string2)
-ciphertext = obj.encrypt(message)
-print("This is the encrypted message: ", ciphertext)
-print(rand_string)
-#asciiKey = str(rand_string,'utf-8')
-asciiKey = rand_string.decode()
-print (asciiKey)
+	return ciphertext
 
+def decryption(cipher):
+	ciphertext = input('Please enter your message: ')
+	
+	key = input('Please enter your key: ')
+	keyLength = 16 - (len(key)%16)
+	x = len(key)
+	for x in range(keyLength):
+		key += " "
+	print(key)
+	obj2 = AES.new(key,AES.MODE_CBC,'This is an IV456')
+	plain_text = obj2.decrypt(cipher)
+	plain_text = plain_text.decode()
+	print(plain_text)
+	
+	
+cipher = encryption()
 
-obj2 = AES.new(asciiKey,AES.MODE_CBC,rand_string2)
-plain_text = obj2.decrypt(ciphertext)
-plain_text = plain_text.decode()
-
-print(plain_text)
+decryption(cipher);
