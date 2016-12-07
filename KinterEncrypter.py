7b375ce76bcea6910961a898539506a5ad1baf45
ryanlb22 <rlebon@csumb.edu>
2016-12-07 10:41:49 -0800

    update

--- KinterEncrypter.py
+++ KinterEncrypter.py
@@ -14,11 +14,13 @@ import numpy as np
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-rand_string2 = os.urandom(16) 
+rand_string = binascii.b2a_hex(os.urandom(16))
+rand_string2 = os.urandom(16)
 
 LARGE_FONT = ("Verdana", 12)
-small_font = ("Verdana", 11)
-
+small_font = ("Verdana", 12)
+hella_small_font = ("Verdana", 5)
+ciphertext = " "
 class KinterEncrypter(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -70,7 +72,7 @@ class StartPage(tk.Frame):
             cv2.destroyAllWindows()
         label = ttk.Label(self, text = "Kinter Encrypter", font = LARGE_FONT)
         label.pack(pady = 10, padx = 10)
-        button1 = ttk.Button(self, text = "Start", command = openCam)
+        button1 = ttk.Button(self, text = "Start", command = lambda:controller.show_frame(MainPage))
         button1.pack()
         quitButton1 = ttk.Button(self,text="Quit", command = label.quit)
         quitButton1.place(x=15,y=300)
@@ -83,25 +85,34 @@ class StartPage(tk.Frame):
 
 class MainPage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        #####THIS IS HOW DEFINITIONS SHOULD BE DONE
+        tk.Frame.__init__(self, parent)
         def encryptMessage():
-            rand_string = binascii.b2a_hex(os.urandom(16))
-            global rand_string2
-            global messsage1
-            message1 = entryEncrypt.get()
-            print(message1)
+            message = entryEncrypt.get()
+            length = 16-(len(message)%16)
+            x = len(message)
+            for x in range(length):
+                message += "_"
+            print(message)
             key = keyEntry.get()
-            obj = AES.new(key,AES.MODE_CFB,rand_string2)
-            ciphertext=obj.encrypt(message1)
-            print("This is the encrypted message: ", ciphertext)
+            print(key)
+            obj = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+            ciphertext = obj.encrypt(message)
+            print(ciphertext)
+
         def decryptMessage():
-            global message2
-            message2 = entryDecrypt.get()
-            key2 = keyEntry.get()
-            obj2 = AES.new(key2, AES.MODE_CFB, rand_string2)
-            decryptedMessage = obj2.decrypt(message2)
-            print("This is the decrypted message: ", obj2.decrypt(message2))
+            key = keyEntry.get()
+            message = entryDecrypt.get()
+            length = 16-(len(message)%16)
+            x = len(message)
+            for x in range(length):
+                message += "_"
+            obj2 = AES.new(key, AES.MODE_CBC, 'This is an IV456')
+            ciphertext = obj2.decrypt(message)
+            print(ciphertext)
+
+
+
+
         tk.Frame.__init__(self, parent)
         #frame=Frame(master)
         #frame.pack()
@@ -123,7 +134,7 @@ class MainPage(tk.Frame):
         keyLabel = ttk.Label(self, text="Key.:", font=small_font)
         keyLabel.place(x=205,y=33)
         entryEncrypt.place(x = 100, y = 18)
-        keyEntry.place(x=250,y=33)
+        keyEntry.place(x=255,y=33)
         entryDecrypt.place(x=100,y=52)
         
     def imageHider(self):
--- encryption.py
+++ encryption.py
@@ -1,8 +1,8 @@
 from Crypto.Cipher import AES
 import os, binascii
 
-rand_string = binascii.b2a_hex(os.urandom(16))
 rand_string2 = os.urandom(16)
+rand_string = binascii.b2a_hex(os.urandom(16))
 
 message =input('Please enter your message: ')
 length = 16 - (len(message)%16)
@@ -14,14 +14,13 @@ for x in range (length):
 obj = AES.new(rand_string,AES.MODE_CBC,rand_string2)
 ciphertext = obj.encrypt(message)
 print("This is the encrypted message: ", ciphertext)
-print(rand_string)
+#print(rand_string)
 #asciiKey = str(rand_string,'utf-8')
 asciiKey = rand_string.decode()
-print (asciiKey)
+print ("This is the message ",asciiKey)
 
 
 obj2 = AES.new(asciiKey,AES.MODE_CBC,rand_string2)
 plain_text = obj2.decrypt(ciphertext)
 plain_text = plain_text.decode()
-
 print(plain_text)
