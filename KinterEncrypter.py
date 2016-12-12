9c4dd1602ed6cf6a0dbdcd8b08fbaccb4927283b
Phillip <philemmons@users.noreply.github.com>
2016-12-11 18:47:00 -0800

    Update KinterEncrypter.py

--- KinterEncrypter.py
+++ KinterEncrypter.py
@@ -1,9 +1,14 @@
 '''
+Title: CST205 project 3 - KinterEncrypter
+Abstract: Haar recogition startup/ ASE keyword initialize main/ 'Hide and Seek' a text message in a image.
 Ryan - GUI/ def encryptMessage
 Kieran - GUI/ def decryptMessage
 Phil -  def imageHider
+Due: 12.11.16
 
-reference:
+
+
+reference
     Buchanan, C., Ip, T., Mabbitt, A., May, B., & Mound, D. (2015). 
     Python web penetration testing cookbook: Over 60 indispensable Python recipes 
     to ensure you always have the right code on hand for web application testing. 
